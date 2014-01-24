5a0b7c754978c2a182dd640b80c4a82bef4a693f
Aleksi Pekkala <aleksipekkala@hotmail.com>
2014-01-24 19:31:37 +0200

    Tutoriaalia eteenpain.

--- game.py
+++ game.py
@@ -7,11 +7,18 @@ Draws heavily from http://steveasleep.com/pyglettutorial.html
 import pyglet
 
 game_window = pyglet.window.Window(800, 600)
-
 main_batch = pyglet.graphics.Batch()
-score_label = pyglet.text.Label(
-    text="Score: 0", x=10, y=575, batch=main_batch)
+game_objects = []
 
+score_label = pyglet.text.Label(
+    text='Score: 0', x=10, y=575, batch=main_batch)
+
+
+def init():
+    """
+    Initializes the game.
+    """
+    game_objects.append()
 
 @game_window.event
 def on_draw():
@@ -19,15 +26,18 @@ def on_draw():
     Draws the game world.
     """
     game_window.clear()
-
     score_label.draw()
+
 
 def update(dt):
     """
     Updates the game world by given timestep.
     """
-    pass
+    for obj in game_objects:
+        obj.update(dt)
+
 
 if __name__ == '__main__':
+    init()
     pyglet.clock.schedule_interval(update, 1/120.0)
     pyglet.app.run()

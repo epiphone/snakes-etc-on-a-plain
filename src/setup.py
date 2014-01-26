83c380325e6cb5ef82f135636614c2e346c57c9e
Olli K <gildean@salaliitto.com>
2014-01-26 20:35:59 +0200

    Add all resources on py2exe build as well

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,32 @@
+import os
+import re
 from distutils.core import setup
 import py2exe
 
+datafiles = []
+
+for files in os.listdir('./resources/'):
+    f1 = './resources/' + files
+    if os.path.isfile(f1):
+        f2 = 'resources', [f1]
+        datafiles.append(f2)
+    else:
+        if files == 'audio':
+            for filest in os.listdir('./resources/audio/'):
+                f2 = './resources/audio/' + filest
+                if os.path.isfile(f2):
+                    f3 = 'resources/audio', [f2]
+                    datafiles.append(f3)
+
+for files in os.listdir('./'):
+    matchObj = re.search( r'map\d.txt', files, re.M|re.I)
+    f1 = './' + files
+    if matchObj:
+        f2 = '', [f1]
+        datafiles.append(f2)
+
 setup(windows=['game.py'],
-      data_files = [('', ['map0.txt', 'map1.txt', 'map2.txt', 'map3.txt', 'map4.txt', 'map5.txt', 'map6.txt'])],
+      data_files = datafiles,
       name="snakes-etc-on-a-plain",
       options={
         "py2exe": {

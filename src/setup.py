dcb10632135b0ce531e3b9ec3c70fc8b4d9c135c
Olli K <gildean@salaliitto.com>
2014-01-26 20:04:38 +0200

    Add missing datafiles to setup.py and change the name

--- src/setup.py
+++ src/setup.py
@@ -2,8 +2,12 @@ from distutils.core import setup
 import py2exe
 
 setup(windows=['game.py'],
+      data_files = [('', ['map0.txt', 'map1.txt', 'map2.txt', 'map3.txt', 'map4.txt', 'map5.txt', 'map6.txt'])],
+      name="snakes-etc-on-a-plain",
       options={
         "py2exe": {
-            "dist_dir": "../release/win32"
+            "dist_dir": "../release/win32",
+            "optimize": 2,
+            "bundle_files": 2
         }
 })

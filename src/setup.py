4de60a459e8311df18fb99407d810add4ff09b90
Olli K <gildean@salaliitto.com>
2014-01-27 06:03:45 +0200

    Add icon, set name of the exe, add build instructions to readme and make sure the built .exe contains the python binary as well

--- src/setup.py
+++ src/setup.py
@@ -4,34 +4,35 @@ from distutils.core import setup
 import py2exe
 
 datafiles = []
+dirs = ['', 'resources', 'resources/audio']
+for directory in dirs:
+    dirname = './' + directory
+    for files in os.listdir(dirname):
+        if (directory == ''):
+            matchObj = re.search( r'map\d.txt', files, re.M|re.I)
+            f1 = dirname + files
+            if matchObj:
+                f2 = directory, [f1]
+                datafiles.append(f2)
+        else:
+            f1 =  dirname + '/' + files
+            if os.path.isfile(f1):
+                f2 = directory, [f1]
+                datafiles.append(f2)
 
-for files in os.listdir('./resources/'):
-    f1 = './resources/' + files
-    if os.path.isfile(f1):
-        f2 = 'resources', [f1]
-        datafiles.append(f2)
-    else:
-        if files == 'audio':
-            for filest in os.listdir('./resources/audio/'):
-                f2 = './resources/audio/' + filest
-                if os.path.isfile(f2):
-                    f3 = 'resources/audio', [f2]
-                    datafiles.append(f3)
-
-for files in os.listdir('./'):
-    matchObj = re.search( r'map\d.txt', files, re.M|re.I)
-    f1 = './' + files
-    if matchObj:
-        f2 = '', [f1]
-        datafiles.append(f2)
-
-setup(windows=['game.py'],
+setup(windows = [{
+        'script': 'game.py',
+        'dest_base': 'snakes-etc-on-a-plain',
+        'icon_resources': [(0, 'resources/icons/icon.ico')]
+      }],
+      name = 'snakes-etc-on-a-plain',
       data_files = datafiles,
-      name="snakes-etc-on-a-plain",
-      options={
-        "py2exe": {
-            "dist_dir": "../release/win32",
-            "optimize": 2,
-            "bundle_files": 2
-        }
+      options = {
+        'py2exe': {
+            'dist_dir': '../release/win32',
+            'dll_excludes': ['w9xpopen.exe'],
+            'bundle_files': 1,
+            'optimize': 2
+      }
 })
+
